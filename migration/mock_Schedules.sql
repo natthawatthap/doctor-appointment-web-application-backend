1a11294688c5e7740e2cb06327bf4f1a9785a163
natthawatthap <natthawatthap@gmail.com>
2023-11-06 04:45:34 +0700

    add mock

---
+++ migration/mock_Schedules.sql
@@ -0,0 +1,51 @@
+INSERT INTO "Schedules"
+(id, "date", times, "doctorName", "location", specialty, "createdAt", "updatedAt")
+VALUES('c7db6ad7-2b44-4b0d-9b7f-2f0c2c0fb5d1'::uuid, '2023-11-05', '{''08:00:00'',''09:00:00'',''10:00:00''}', 'Petch Jirapinyo', 'Location A', 'Specialty A', '2023-11-05 04:50:04.449', '2023-11-05 04:50:04.449');
+INSERT INTO "Schedules"
+(id, "date", times, "doctorName", "location", specialty, "createdAt", "updatedAt")
+VALUES('c3db6ad7-1b44-4a0d-8b7f-2f0c2c0fb5d1'::uuid, '2023-11-07', '{''11:00:00'',''12:00:00''}', 'Ping Tuksaorn', 'Location C', 'Specialty C', '2023-11-05 04:50:04.449', '2023-11-05 04:50:04.449');
+INSERT INTO "Schedules"
+(id, "date", times, "doctorName", "location", specialty, "createdAt", "updatedAt")
+VALUES('f128cd64-88d5-3d0d-7c8a-82008ec6e99c'::uuid, '2023-11-08', '{''14:00:00'',''15:00:00''}', 'Jay Tanyatorn', 'Location D', 'Specialty D', '2023-11-05 04:50:04.449', '2023-11-05 04:50:04.449');
+INSERT INTO "Schedules"
+(id, "date", times, "doctorName", "location", specialty, "createdAt", "updatedAt")
+VALUES('a9ef2d5d-3d6f-4339-9a21-8bfa8e2d2e15'::uuid, '2023-11-09', '{''17:00:00''}', 'Yok Kanya', 'Location E', 'Specialty E', '2023-11-05 04:50:04.449', '2023-11-05 04:50:04.449');
+INSERT INTO "Schedules"
+(id, "date", times, "doctorName", "location", specialty, "createdAt", "updatedAt")
+VALUES('bcbaf56a-30d7-4d18-8f6b-8fc82ad6c261'::uuid, '2023-11-10', '{''10:00:00'',''11:00:00''}', 'Three Thanwarin', 'Location F', 'Specialty F', '2023-11-05 04:50:04.449', '2023-11-05 04:50:04.449');
+INSERT INTO "Schedules"
+(id, "date", times, "doctorName", "location", specialty, "createdAt", "updatedAt")
+VALUES('b6f9cf43-eb0c-4f8d-8626-41188d54b6a3'::uuid, '2023-11-11', '{''13:00:00'',''14:00:00''}', 'Chelsea D’Ascanio', 'Location G', 'Specialty G', '2023-11-05 04:50:04.449', '2023-11-05 04:50:04.449');
+INSERT INTO "Schedules"
+(id, "date", times, "doctorName", "location", specialty, "createdAt", "updatedAt")
+VALUES('4bf9cf43-eb0c-4f8d-8626-41188d54b6a4'::uuid, '2023-11-12', '{''16:00:00'',''17:00:00''}', 'Sheri Zarella', 'Location H', 'Specialty H', '2023-11-05 04:50:04.449', '2023-11-05 04:50:04.449');
+INSERT INTO "Schedules"
+(id, "date", times, "doctorName", "location", specialty, "createdAt", "updatedAt")
+VALUES('d6f9cf43-eb0c-4f8d-8626-41188d54b6a5'::uuid, '2023-11-13', '{''19:00:00''}', 'Carmen McWilliams', 'Location I', 'Specialty I', '2023-11-05 04:50:04.449', '2023-11-05 04:50:04.449');
+INSERT INTO "Schedules"
+(id, "date", times, "doctorName", "location", specialty, "createdAt", "updatedAt")
+VALUES('e6f9cf43-eb0c-4f8d-8626-41188d54b6a6'::uuid, '2023-11-14', '{''08:00:00'',''09:00:00'',''10:00:00''}', 'Lauren Kort', 'Location J', 'Specialty J', '2023-11-05 04:50:04.449', '2023-11-05 04:50:04.449');
+INSERT INTO "Schedules"
+(id, "date", times, "doctorName", "location", specialty, "createdAt", "updatedAt")
+VALUES('f6f9cf43-eb0c-4f8d-8626-41188d54b6a7'::uuid, '2023-11-15', '{''11:00:00'',''12:00:00''}', 'Zlatan (Z) Harambasic', 'Location K', 'Specialty K', '2023-11-05 04:50:04.449', '2023-11-05 04:50:04.449');
+INSERT INTO "Schedules"
+(id, "date", times, "doctorName", "location", specialty, "createdAt", "updatedAt")
+VALUES('1bf9cf43-eb0c-4f8d-8626-41188d54b6a8'::uuid, '2023-11-16', '{''14:00:00'',''15:00:00''}', 'Janelle Esker', 'Location L', 'Specialty L', '2023-11-05 04:50:04.449', '2023-11-05 04:50:04.449');
+INSERT INTO "Schedules"
+(id, "date", times, "doctorName", "location", specialty, "createdAt", "updatedAt")
+VALUES('2bf9cf43-eb0c-4f8d-8626-41188d54b6a9'::uuid, '2023-11-17', '{''17:00:00''}', 'Grayce O’Neill', 'Location M', 'Specialty M', '2023-11-05 04:50:04.449', '2023-11-05 04:50:04.449');
+INSERT INTO "Schedules"
+(id, "date", times, "doctorName", "location", specialty, "createdAt", "updatedAt")
+VALUES('3bf9cf43-eb0c-4f8d-8626-41188d54b6b0'::uuid, '2023-11-18', '{''09:00:00'',''10:00:00''}', 'Shivani Mehta', 'Location N', 'Specialty N', '2023-11-05 04:50:04.449', '2023-11-05 04:50:04.449');
+INSERT INTO "Schedules"
+(id, "date", times, "doctorName", "location", specialty, "createdAt", "updatedAt")
+VALUES('4bf9cf43-eb0c-4f8d-8626-41188d54b6b1'::uuid, '2023-11-19', '{''12:00:00'',''13:00:00''}', 'Christopher C. Thompson', 'Location O', 'Specialty O', '2023-11-05 04:50:04.449', '2023-11-05 04:50:04.449');
+INSERT INTO "Schedules"
+(id, "date", times, "doctorName", "location", specialty, "createdAt", "updatedAt")
+VALUES('5bf9cf43-eb0c-4f8d-8626-41188d54b6b2'::uuid, '2023-11-20', '{''15:00:00'',''16:00:00''}', 'Pichamol Jirapinyo', 'Location P', 'Specialty P', '2023-11-05 04:50:04.449', '2023-11-05 04:50:04.449');
+INSERT INTO "Schedules"
+(id, "date", times, "doctorName", "location", specialty, "createdAt", "updatedAt")
+VALUES('c7db6ad7-2b44-4b0d-9b7f-2f0c2c0fb5d2'::uuid, '2023-11-06', '{''08:00:00'',''09:00:00'',''10:00:00''}', 'Petch Jirapinyo', 'Location A', 'Specialty A', '2023-11-05 04:50:04.449', '2023-11-05 04:50:04.449');
+INSERT INTO "Schedules"
+(id, "date", times, "doctorName", "location", specialty, "createdAt", "updatedAt")
+VALUES('e128cd64-48d5-4d0d-9c8a-82008ec6e99c'::uuid, '2023-12-06', '{''09:00:00'',''10:00:00'',''11:00:00''}', 'Chuck DiNolfo', 'Location B', 'Specialty B', '2023-11-05 04:50:04.449', '2023-11-05 04:50:04.449');
